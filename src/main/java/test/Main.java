c404022cd18ebd6fd9d90effc23b84d858959109
Max Zinovev <zinkacartinka765@gmail.com>
2023-10-23 17:21:46 +0300

    first version of a project. Working DB and Javafx

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,34 @@
+package test;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        System.out.printf("hii");
+        launch(args);
+
+
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Загрузка FXML-файла из ресурсов
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/hello-view.fxml"));
+        Parent root = loader.load();
+
+        // Создание сцены
+        Scene scene = new Scene(root, 700, 400);
+
+        // Настройка и отображение окна
+        primaryStage.setTitle("CINEMA");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+
+
+}
---
+++ src/main/java/test/Movie.java
@@ -0,0 +1,70 @@
+package test;
+
+import javax.persistence.*;
+import java.security.PublicKey;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+    private int movieId;
+    private String movieName;
+    private String genre;
+    private int year;
+    private String director;
+    private static int[] inceptionDate = new int[3];
+    private static int[] finalDate = new int[3];
+
+    public Movie(){
+
+    }
+
+    @Id
+    @Column(name = "movie_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getMovieId(){
+        return movieId;
+    }
+    public void setMovieId (int newMovieId){
+        movieId = newMovieId;
+    }
+
+    @Column(name="movie_title")
+    public String getMovieName(){
+        return movieName;
+    }
+   public void setMovieName(String newTitle){
+        movieName = newTitle;
+    }
+
+    @Column(name="movie_year")
+    public int getYear(){
+        return year;
+    }
+    public void setYear(int newYear){
+        year = newYear;
+    }
+
+    @Column(name="movie_genre")
+    public String getGenre(){
+        return genre;
+    }
+    public void setGenre(String newGenre){
+        genre = newGenre;
+    }
+    @Column(name="movie_director")
+    public String getDirector(){
+        return director;
+    }
+    public void setDirector(String newDirector){
+        director = newDirector;
+    }
+    @Column(name = "movie_inceptionDate")
+    public static int[] getIncDate (){
+        return inceptionDate;
+    }
+    @Column(name = "movie_finalDate")
+    public static int[] getFinDate (){
+        return finalDate;
+    }
+
+}
